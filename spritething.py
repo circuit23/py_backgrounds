01bcbc5cfed5b18c56e42e0d04b4b77eca29a093
c23 <circuit.23@gmail.com>
2024-08-02 11:49:10 -0400

    fixing issues- they weren't filling the image, also weren't symmetrical

--- spritething.py
+++ spritething.py
@@ -4,16 +4,15 @@ import sys
 from PIL import Image, ImageDraw
 
 origDimension = 1500
-
 r = lambda: random.randint(50, 215)
 rc = lambda: (r(), r(), r())
 listSym = []
 
 
 def create_square(border, draw, randColor, element, size):
-    if element == int(size / 2):
+    if (element == int(size / 2)):
         draw.rectangle(border, randColor)
-    elif len(listSym) == element + 1:
+    elif (len(listSym) == element + 1):
         draw.rectangle(border, listSym.pop())
     else:
         listSym.append(randColor)
@@ -26,7 +25,7 @@ def create_invader(border, draw, size):
     randColors = [rc(), rc(), rc(), (0, 0, 0), (0, 0, 0), (0, 0, 0)]
     i = 1
     for y in range(0, size):
-        i += -1
+        i *= -1
         element = 0
         for x in range(0, size):
             topLeftX = x * squareSize + x0
@@ -34,9 +33,9 @@ def create_invader(border, draw, size):
             botRightX = topLeftX + squareSize
             botRightY = topLeftY + squareSize
             create_square((topLeftX, topLeftY, botRightX, botRightY), draw, random.choice(randColors), element, size)
-            if element == int(size / 2) or element == 0:
-                i *= -1
-                element += i
+            if (element == int(size / 2) or element == 0):
+                i *= -1;
+            element += i
 
 
 def main(size, invaders, imgSize):
@@ -51,10 +50,10 @@ def main(size, invaders, imgSize):
             topLeftY = y * invaderSize + padding / 2
             botRightX = topLeftX + invaderSize - padding
             botRightY = topLeftY + invaderSize - padding
-        create_invader((topLeftX, topLeftY, botRightX, botRightY), draw, size)
+            create_invader((topLeftX, topLeftY, botRightX, botRightY), draw, size)
     origImage.save(
         "Examples/Example-" + str(size) + "x" + str(size) + "-" + str(invaders) + "-" + str(imgSize) + ".jpg")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
